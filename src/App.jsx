73650fb7b063dd09d652d90321eb7ac237e59ebf
IsraelSoftTech <iminuifuong@gmail.com>
2026-02-19 14:10:20 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaLeaf } from 'react-icons/fa';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FaLeaf className="App-logo" />
+        <h1>DigiLync</h1>
+        <p>WhatsApp-first agricultural coordination platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
